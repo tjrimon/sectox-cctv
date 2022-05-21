4b37b52ba098e9093e09b1e69d624364fe5d0920
tjrimon <tjrimonr@gmail.com>
2022-05-21 15:11:58 +0600

    react router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import "./App.css";
 import Navbar from "./components/Sections/Navbar/Navbar";
 import Home from "./components/Pages/Home/Home";
 import Footer from "./components/Sections/Footer/Footer";
+import { Route, Routes } from "react-router-dom";
+import NotFound from "./components/Sections/NotFound/NotFound";
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
       <Footer></Footer>
     </div>
   );
---
+++ src/components/Sections/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="py-20 flex justify-center items-center font-bold text-3xl">
+      404 Not Found
+    </div>
+  );
+};
+
+export default NotFound;
